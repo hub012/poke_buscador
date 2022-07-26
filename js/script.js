15079fa0c50ceddcda3caed8c1c1bf77020cd21f
Abel Quinones <hub0121@gmail.com>
2022-07-26 14:21:10 -0400

    creacion de funcion y materia

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
 const API_URL = 'https://pokeapi.co/api/v2';
-const div = document.querySelector('#app');
-const img = document.querySelector('#frontal');
+const div = document.querySelector('#moves');
+const img = document.querySelector('#character');
+const skills_div =document.querySelector('#skills');
+const stats_div =document.querySelector('#stats');
 
+// 1. creacion de boton de busqueda.
+// 2. implementar la funcionalidad de busqueda.
+//2.1 cada vez que le demos a buscar apareca los datos del pokemon.
+// 2.2 buscar llamar funcion desde el boton de html 
+// 3. implemtar materializer.
 
-
-function buscador(pokemon){
-    return fetch(`${API_URL}/pokemon/jolteon`)
+async function buscador(){
+    var pokemon_name = document.getElementById("name").value;
+    console.log (pokemon_name);
+    return await fetch(`${API_URL}/pokemon/`+ pokemon_name)
     .then((response) => response.json())
     .then((pokemon) => {
         console.log(pokemon);
         const cadena_nombre = `<li>${pokemon.name}</li>`;
-        const cadena_movimientos = pokemon.moves.map((mov) => `<li>${mov.move.name} </li>`);
-        const cadena_skills = pokemon.abilities.map((skill) => `<li>${skill.ability.name} </li>`);
-        const cadena_stats = pokemon.stats.map((stat) => `<li>${stat.stat.name} </li>`);
+        const cadena_movimientos = pokemon.moves.slice(0,5).map((mov) => `<li>${mov.move.name}</li>`).join('');
+        const cadena_skills = pokemon.abilities.map((skill) => `<li>${skill.ability.name} </li>`).join('');
+        const cadena_stats = pokemon.stats.map((statistic) => `<li>${statistic.stat.name}: ${statistic.base_stat} </li>`).join('');
         const cadena_front_default =pokemon.sprites.front_default;
-        div.innerHTML = `<o1>${cadena_front_default}</o1>`;
+        div.innerHTML = cadena_movimientos;
+        skills_div.innerHTML = cadena_skills;
+        stats_div.innerHTML = cadena_stats;
         img.setAttribute("src", cadena_front_default);
     });
 
